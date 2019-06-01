ff58aa1d9122d05ee24a6397a65ee52717aa72ec
temkafox <temkafox@mail.ru>
2019-06-01 23:13:59 +0300

    1

--- app/views.py
+++ app/views.py
@@ -16,7 +16,7 @@ def data():
 @app.route('/req_items', methods=['POST'])
 def req_items():
 	a = request.args.get('id')
-	print("то что получает" + a)
+	print("то что получает", a)
 	return 'test'
 #@app.route('/index')
 #def main():
