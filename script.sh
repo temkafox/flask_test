80306f6827ade00fe0ce1cbb82223df37ff5f9b7
temkafox <temkafox@mail.ru>
2019-06-01 22:09:39 +0000

    123

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 /etc/init.d/mysql restart
-mysql -u root -p <<MY_QUERY
+mysql -uroot <<MY_QUERY
 CREATE DATABASE langs;
 USE langs;
 MY_QUERY
-mysql langs -uroot -p < /flask_test/db
-mysql -u root -p <<USER_ROOT
+mysql langs -uroot < /flask_test/db
+mysql -uroot <<USER_ROOT
 CREATE USER 'root1'@'localhost' IDENTIFIED BY '';
 GRANT ALL PRIVILEGES ON *.* TO 'root1'@'localhost';
 UPDATE user SET plugin='auth_socket' WHERE User='root1';
