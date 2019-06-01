a32ffdcbf36266f3edba824519c57cb2cf3f0f38
temkafox <temkafox@mail.ru>
2019-06-01 19:13:37 +0300

    123

--- app/config.py
+++ app/config.py
@@ -1,2 +1,2 @@
 class Configuration(object):
-	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/db'
+	SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:@localhost/langs'
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 #!flask/bin/python
 from app import app
+from config import Configuration
 app.run(debug = True, host='0.0.0.0')
