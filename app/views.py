c67b8d996525458cdd0b9e2fef3e6ae1bc9c82e7
temkafox <temkafox@mail.ru>
2019-06-01 21:32:28 +0300

    123

--- app/views.py
+++ app/views.py
@@ -6,10 +6,9 @@ from flask import render_template
 @app.route('/')
 def data():
 	cursor = db.cursor()
-    sql = 'SELECT * FROM lang'
-    cursor.execute(sql)
-    results = cursor.fetchall()
-
+	sql = 'SELECT * FROM langs'
+	cursor.execute(sql)
+	results = cursor.fetchall()
 @app.route('/index')
 def main():
 	data()
