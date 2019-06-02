18dd286f42aa0187f2b52869f1434de3d873834c
temkafox <temkafox@mail.ru>
2019-06-02 15:09:14 +0300

    1

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 /etc/init.d/mysql restart
-mysql -uroot -e "CREATE DATABASE langs; USE langs;CREATE USER 'root1'@'localhost' IDENTIFIED BY '';GRANT ALL PRIVILEGES ON *.* TO 'root1'@'localhost';UPDATE user SET plugin='auth_socket' WHERE User='root1';FLUSH PRIVILEGES;" < /flask_test/db
+mysql -uroot -e "CREATE DATABASE langs; USE langs;" < /flask_test/db
 
 #mysql -uroot -e "CREATE DATABASE langs; USE langs;"
 
@@ -10,11 +10,11 @@ mysql -uroot -e "CREATE DATABASE langs; USE langs;CREATE USER 'root1'@'localhost
 #MY_QUERY
 #mysql langs -uroot < /flask_test/db
 
-#mysql -uroot <<USER_ROOT
-#CREATE USER 'root1'@'localhost' IDENTIFIED BY '';
-#GRANT ALL PRIVILEGES ON *.* TO 'root1'@'localhost';
-#UPDATE user SET plugin='auth_socket' WHERE User='root1';
-#FLUSH PRIVILEGES;
-#USER_ROOT
+mysql -uroot <<USER_ROOT
+CREATE USER 'root1'@'localhost' IDENTIFIED BY '';
+GRANT ALL PRIVILEGES ON *.* TO 'root1'@'localhost';
+UPDATE user SET plugin='auth_socket' WHERE User='root1';
+FLUSH PRIVILEGES;
+USER_ROOT
 
-#python /flask_test/run.py
+python /flask_test/run.py
